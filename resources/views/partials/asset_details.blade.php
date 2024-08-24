f55e3acfe2816310677df7e4a59deb5ad3d85eba
Dave Flores Malhin <malhindave@gmail.com>
2024-08-25 04:10:03 +0800

    Remove details lang sa

---
+++ resources/views/partials/asset_details.blade.php
@@ -0,0 +1,39 @@
+<div class="row mb-3">
+    <div class="col-12">
+        <div class="d-flex justify-content-between align-items-center">
+            <label class="eventtitle">Asset Details</label>
+        </div>
+    </div>
+</div>
+
+<div class="row mb-3">
+    <!-- Miscellaneous Section -->
+    <div class="col-md-6">
+        <div class="form-group">
+            <label for="serialNo" class="form-label">Serial No</label>
+            <input type="text" class="form-control" id="serialNo" value="sd" readonly>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <label for="purchasedFrom" class="form-label">Purchased from</label>
+            <input type="text" class="form-control" id="purchasedFrom" value="sd" readonly>
+        </div>
+    </div>
+</div>
+
+<div class="row mb-3">
+    <!-- Creation Section -->
+    <div class="col-md-6">
+        <div class="form-group">
+            <label for="dateCreated" class="form-label">Date Created</label>
+            <input type="text" class="form-control" id="dateCreated" value="06/24/2024 06:05 AM" readonly>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <label for="createdBy" class="form-label">Created by</label>
+            <input type="text" class="form-control" id="createdBy" value="David Flores" readonly>
+        </div>
+    </div>
+</div>
